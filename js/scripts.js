8472e1fd067e0f75422eda9abb43405ca4c21a2e
Ivan Glinka <79760374+ivanh1ynka@users.noreply.github.com>
2023-01-04 00:44:50 +0200

    it-team v-1.0

---
+++ js/scripts.js
@@ -0,0 +1,45 @@
+// Custom Scripts
+// Custom scripts
+// Мобильное меню бургер
+function burgerMenu() {
+  const burger = document.querySelector('.burger')
+  const menu = document.querySelector('.menu')
+  const body = document.querySelector('body')
+  burger.addEventListener('click', () => {
+    if (!menu.classList.contains('active')) {
+      menu.classList.add('active')
+      burger.classList.add('active-burger')
+      body.classList.add('locked')
+    } else {
+      menu.classList.remove('active')
+      burger.classList.remove('active-burger')
+      body.classList.remove('locked')
+    }
+  })
+  // Вот тут мы ставим брейкпоинт навбара
+  window.addEventListener('resize', () => {
+    if (window.innerWidth > 991.98) {
+      menu.classList.remove('active')
+      burger.classList.remove('active-burger')
+      body.classList.remove('locked')
+    }
+  })
+}
+burgerMenu()
+
+
+// Вызываем эту функцию, если нам нужно зафиксировать меню при скролле.
+function fixedHeader() {
+  const header = document.querySelector('header')
+
+  // тут указываем в пикселях, сколько нужно проскроллить что бы наше меню стало фиксированным
+  const breakpoint = 100
+  if (window.scrollY >= breakpoint) {
+    header.classList.add('fixed__header')
+  } else {
+    header.classList.remove('fixed__header')
+  }
+}
+window.addEventListener('scroll', fixedHeader)
+;
+
